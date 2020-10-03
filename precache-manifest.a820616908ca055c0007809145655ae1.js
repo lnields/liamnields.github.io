7eb4ed772c56b22d4282b859ff1361e22724185d
lnields <53065285+lnields@users.noreply.github.com>
2020-10-03 16:14:11 -0400

    Updates

---
+++ precache-manifest.a820616908ca055c0007809145655ae1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6103354e5c71f5df767",
+    "url": "/static/css/main.c72ebabc.chunk.css"
+  },
+  {
+    "revision": "f6103354e5c71f5df767",
+    "url": "/static/js/main.06e3b41f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6ebd3a1e0819a99310e",
+    "url": "/static/js/2.9617cd25.chunk.js"
+  },
+  {
+    "revision": "c329fe2130f995e095b9862b18ff0fab",
+    "url": "/static/media/galaxy.c329fe21.png"
+  },
+  {
+    "revision": "319c63a5020158a503b511480d23f30e",
+    "url": "/static/media/surfer.319c63a5.png"
+  },
+  {
+    "revision": "82f99acfebb8e2c150f5e69aa2644b81",
+    "url": "/static/media/data.82f99acf.png"
+  },
+  {
+    "revision": "c12ea95582f767ae4cdee55273f958ba",
+    "url": "/static/media/dance.c12ea955.png"
+  },
+  {
+    "revision": "218f9090ff39403813e53ba889ca0eb9",
+    "url": "/static/media/roast-bot-logo.218f9090.png"
+  },
+  {
+    "revision": "b5041d9ba2f6f0fa90626e68a375777d",
+    "url": "/index.html"
+  }
+];
